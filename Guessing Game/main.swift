72a3e9f4c3ca0d7a0e3652f3bd0823c30cf425e2
Zach Shearer <zachshearer@interapts-macbook-pro-7.local>
2018-08-16 10:22:42 -0500

    Initial Commit

---
+++ Guessing Game/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Guessing Game
+//
+//  Created by Zach Shearer on 8/16/18.
+//  Copyright © 2018 Zach Shearer. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
